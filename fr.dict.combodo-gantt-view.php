821816de239c193f8c8abf7bca672f0104e34cfb
acognet <anne-catherine.cognet@combodo.com>
2020-06-23 13:49:52 +0200

    Spelling correction

--- fr.dict.combodo-gantt-view.php
+++ fr.dict.combodo-gantt-view.php
@@ -5,7 +5,6 @@
  * @copyright   Copyright (C) 2020 Combodo SARL
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
-
 Dict::Add('FR FR', 'French', 'Français', array(
 	// Dictionary entries go here
 	'GanttDashlet/Label' => 'Gantt',
@@ -24,9 +23,8 @@ Dict::Add('FR FR', 'French', 'Français', array(
 	'GanttDashlet/Prop:AdditionalInfoLeft' => 'Information complémentaire 1',
 	'GanttDashlet/Prop:AdditionalInfoRight' => 'Information complémentaire 2',
 	'GanttDashlet/Attribute:ActionDisabled' => 'Action impossible sur cet objet',
-	'GanttDashlet:UI/Tab:GanttViewer' => "Diagram de Gantt",
-	'GanttDashlet/Prop:ParentField'=>'Groupé par',
-	'GanttDashlet/Prop:Percentage'=>'Pourcentage d\'avancement',
+	'GanttDashlet:UI/Tab:GanttViewer' => 'Diagram de Gantt',
+	'GanttDashlet/Prop:ParentField' => 'Groupé par',
+	'GanttDashlet/Prop:Percentage' => 'Pourcentage d\'avancement',
 	'GanttDashlet/Prop:GroupByInformations' => 'Informations sur le regroupement',
 ));
-?>
