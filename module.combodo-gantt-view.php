42a8cd92749a953d04a63c6dc85f90aee4d04bb8
acognet <anne-catherine.cognet@combodo.com>
2020-06-25 18:10:56 +0200

    version 1.0.0 to 1.0.1

--- module.combodo-gantt-view.php
+++ module.combodo-gantt-view.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-gantt-view/1.0.0',
+	'combodo-gantt-view/1.0.1',
 	array(
 		// Identification
 		//
