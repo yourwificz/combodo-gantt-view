330ed7261ba1a76368eda6321161949efa7d1487
Molkobain <lajarige.guillaume@free.fr>
2021-12-27 14:20:48 +0100

    Prepare 1.1.0

--- module.combodo-gantt-view.php
+++ module.combodo-gantt-view.php
@@ -10,7 +10,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-gantt-view/1.0.3',
+	'combodo-gantt-view/1.1.0',
 	array(
 		// Identification
 		//
