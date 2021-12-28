yourwificz/combodo-gantt-view

commit e532a625f22cf31bb630a046b07472f59596ee61

Security hardering

src/Dashlet/Gantt.php
@@ -183,13 +183,10 @@ class Gantt
 	{
 		$aRow = array();
 		$aRow['id'] = $sClass.'_'.$oRow->GetKey();
-		$aRow['name'] = $oRow->Get($aFields->sLabel);
-		if ($aFields->sPercentage != null && $aFields->sPercentage != '')
-		{
+		$aRow['name'] = utils::EscapeHtml($oRow->Get($aFields->sLabel));
+		if ($aFields->sPercentage != null && $aFields->sPercentage != '') {
 			$aRow['progress'] = $oRow->Get($aFields->sPercentage);
-		}
-		else
-		{
+		} else {
 			$aRow['progress'] = 0;
 		}
 		$aRow['progressByWorklog'] = true;
@@ -248,14 +245,12 @@ class Gantt
 		}
 		$aRow['end'] = $iEnd;
 		$aRow['duration'] = $iEnd - $iStart;
-		if ($aFields->sAdditionalInformation1 != '')
-		{
+		if ($aFields->sAdditionalInformation1 != '') {
 			//$aGroupBy['group1']->MakeValueLabel($this->m_oFilter, $sStateValue, $sStateValue);
-			$aRow['info1'] = $oRow->Get($aFields->sAdditionalInformation1);
+			$aRow['info1'] = utils::EscapeHtml($oRow->Get($aFields->sAdditionalInformation1));
 		}
-		if ($aFields->sAdditionalInformation2 != '')
-		{
-			$aRow['info2'] = $oRow->Get($aFields->sAdditionalInformation2);
+		if ($aFields->sAdditionalInformation2 != '') {
+			$aRow['info2'] = utils::EscapeHtml($oRow->Get($aFields->sAdditionalInformation2));
 		}
 		$aRow['collapsed'] = true;
 		$aRow['assigs'] = [];

view/GanttViewer.html.twig
@@ -28,21 +28,16 @@
     <div class="__template__" type="TASKEMPTYROW"><!--
 			  <tr class="taskEditRow emptyRow" >
 			    <td class="gdfCell" align="right"></td>
-			    <td class="gdfCell noClip" align="center"></td>
-			    <td class="gdfCell"></td>
-			    <td class="gdfCell"></td>
-			    <td class="gdfCell"></td>
-			    <td class="gdfCell"></td>
-			    <td class="gdfCell"></td>
-			    <td class="gdfCell"></td>
-			    <td class="gdfCell"></td>
-			    <td class="gdfCell"></td>
-			    <td class="gdfCell requireCanSeeDep"></td>
-			    <td class="gdfCell"></td>
+			    <td class="gdfCell noClip"></td>
+				<td class="gdfCell"></td>
+				<td class="gdfCell"></td>
+				<td class="gdfCell"></td>
+				<td class="gdfCell"></td>
+				<td class="gdfCell"></td>
 			  </tr>
 			  --></div>
-		<!--table with 8 columns minimum-->
-		<div class="__template__" type="TASKSEDITHEAD"><!--
+    <!--table with 8 columns minimum-->
+    <div class="__template__" type="TASKSEDITHEAD"><!--
 			<table class="gdfTable" cellspacing="0" cellpadding="0">
 				<thead>
 					<tr style="height:40px">

commit b7162477cf2762d2c2976501193ae177d1d14e3a

Restore compatibility with iTop 2.7 (regression from e532a625f)

src/Dashlet/Gantt.php
@@ -183,7 +183,7 @@ class Gantt
 	{
 		$aRow = array();
 		$aRow['id'] = $sClass.'_'.$oRow->GetKey();
-		$aRow['name'] = utils::EscapeHtml($oRow->Get($aFields->sLabel));
+		$aRow['name'] = htmlspecialchars($oRow->Get($aFields->sLabel), ENT_QUOTES | ENT_DISALLOWED | ENT_HTML5, "UTF-8");
 		if ($aFields->sPercentage != null && $aFields->sPercentage != '') {
 			$aRow['progress'] = $oRow->Get($aFields->sPercentage);
 		} else {
@@ -247,10 +247,10 @@ class Gantt
 		$aRow['duration'] = $iEnd - $iStart;
 		if ($aFields->sAdditionalInformation1 != '') {
 			//$aGroupBy['group1']->MakeValueLabel($this->m_oFilter, $sStateValue, $sStateValue);
-			$aRow['info1'] = utils::EscapeHtml($oRow->Get($aFields->sAdditionalInformation1));
+			$aRow['info1'] = htmlspecialchars($oRow->Get($aFields->sAdditionalInformation1), ENT_QUOTES | ENT_DISALLOWED | ENT_HTML5, "UTF-8");
 		}
 		if ($aFields->sAdditionalInformation2 != '') {
-			$aRow['info2'] = utils::EscapeHtml($oRow->Get($aFields->sAdditionalInformation2));
+			$aRow['info2'] = htmlspecialchars($oRow->Get($aFields->sAdditionalInformation2), ENT_QUOTES | ENT_DISALLOWED | ENT_HTML5, "UTF-8");
 		}
 		$aRow['collapsed'] = true;
 		$aRow['assigs'] = [];