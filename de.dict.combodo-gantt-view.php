32e0ff72c00ffa65a99a41e2972c1c4823578afc
Lars Kaltefleiter <lars@kaltefleiter.org>
2022-06-14 18:46:40 +0200

    :globe_with_meridians: Added German translation (by ITOMIG GmbH) (#1)
    Co-authored-by: David Gümbel <david.guembel@itomig.de>

---
+++ de.dict.combodo-gantt-view.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * @copyright   Copyright (C) 2010-2021 Combodo SARL, 2022 ITOMIG GmbH
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('DE DE', 'German', 'Deutsch', array(
+	// Dictionary entries go here
+	'GanttDashlet/Label' => 'Gantt',
+	'GanttDashlet/Description' => 'Objekte als Gantt-Chart anzeigen',
+	'GanttDashlet/NoPreviewAvailable' => 'Keine Vorschau verfügbar!',
+	'GanttDashlet/Prop:DefaultTitle' => 'Tickets',
+	'GanttDashlet/Prop:DefaultTitle2' => 'Kontakte',
+
+	'GanttDashlet/Prop:Class' => 'Klasse',
+	'GanttDashlet/Prop:Name' => 'Name',
+	'GanttDashlet/Prop:StartDate' => 'Startdatum',
+	'GanttDashlet/Prop:EndDate' => 'Enddatum',
+	'GanttDashlet/Prop:Title' => 'Titel',
+	'GanttDashlet/Prop:DependsOn' => 'Hängt ab von',
+	'GanttDashlet/Prop:ColoredField' => 'Farblich differenziert nach',
+	'GanttDashlet/Prop:AdditionalInfoLeft' => 'Zusätzliche Information 1',
+	'GanttDashlet/Prop:AdditionalInfoRight' => 'Zusätzliche Information 2',
+	'GanttDashlet/Attribute:ActionDisabled' => 'Aktionen für dieses Objekt deaktiviert',
+	'GanttDashlet:UI/Tab:GanttViewer' => "Gantt-Ansicht",
+	'GanttDashlet/Prop:ParentField' => 'Gruppiere nach',
+	'GanttDashlet/Prop:Percentage' => 'Prozentuale Fertigstellung',
+	'GanttDashlet/Prop:GroupByInformations' => 'Gruppiere nach Informationen',
+
+	'GanttDashlet/UI:CriticalPath' => 'Kritischer Pfad',
+	'GanttDashlet/UI:ZoomIn' => 'Hineinzoomen',
+	'GanttDashlet/UI:ZoomOut' => 'Herauszoomen',
+	'GanttDashlet/UI:MoveSplitBarRight' => 'Trennbalken nach rechts verschieben',
+	'GanttDashlet/UI:MoveSplitBarLeft' => 'Trennbalken nach links verschieben',
+	'GanttDashlet/UI:MoveSplitBarCenter' => 'Trennbalken zentrieren',
+
+	'GanttDashlet/Error:ParametersMissing' => 'Es fehlen Parameter, bitte die Konfiguration überprüfen',
+));
