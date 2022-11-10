fe1911142d966e96b42d3b9dc0491e47c92e9d7a
Eric Espie <eric.espie@combodo.com>
2022-11-10 15:48:00 +0100

    Version 1.1.3

--- module.combodo-gantt-view.php
+++ module.combodo-gantt-view.php
@@ -10,7 +10,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-gantt-view/1.1.3-dev',
+	'combodo-gantt-view/1.1.3',
 	array(
 		// Identification
 		//
