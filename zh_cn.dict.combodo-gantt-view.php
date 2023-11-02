3920038b426e52bedc203e690e5d6eaac40976ae
Devin <bdejin@hotmail.com>
2023-11-02 17:01:40 +0800

    N°6894 - Update chinese translations thanks to @bdejin (#2)

---
+++ zh_cn.dict.combodo-gantt-view.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * @copyright   Copyright (C) 2010-2021 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+ Dict::Add('ZH CN', 'Chinese', '简体中文', array(
+	// Dictionary entries go here
+	'GanttDashlet/Label' => '甘特图',
+	'GanttDashlet/Description' => '以甘特图显示对象',
+	'GanttDashlet/NoPreviewAvailable' => '预览不可用!!',
+	'GanttDashlet/Prop:DefaultTitle' => '工单',
+	'GanttDashlet/Prop:DefaultTitle2' => '联系人',
+
+	'GanttDashlet/Prop:Class' => '类型',
+	'GanttDashlet/Prop:Name' => '名称',
+	'GanttDashlet/Prop:StartDate' => '开始日期',
+	'GanttDashlet/Prop:EndDate' => '结束日期',
+	'GanttDashlet/Prop:Title' => '标题',
+	'GanttDashlet/Prop:DependsOn' => '依赖',
+	'GanttDashlet/Prop:ColoredField' => '按颜色',
+	'GanttDashlet/Prop:AdditionalInfoLeft' => '附加信息 1',
+	'GanttDashlet/Prop:AdditionalInfoRight' => '附加信息 2',
+	'GanttDashlet/Attribute:ActionDisabled' => '此项目禁止操作',
+	'GanttDashlet:UI/Tab:GanttViewer' => "甘特视图",
+	'GanttDashlet/Prop:ParentField' => '分组',
+	'GanttDashlet/Prop:Percentage' => '完成比例',
+	'GanttDashlet/Prop:GroupByInformations' => '按信息分组',
+
+	'GanttDashlet/UI:CriticalPath' => '关键路径',
+	'GanttDashlet/UI:ZoomIn' => '放大',
+	'GanttDashlet/UI:ZoomOut' => '缩小',
+	'GanttDashlet/UI:MoveSplitBarRight' => '分隔条居右',
+	'GanttDashlet/UI:MoveSplitBarLeft' => '分隔条居左',
+	'GanttDashlet/UI:MoveSplitBarCenter' => '分隔条居中',
+
+	'GanttDashlet/Error:ParametersMissing' => '缺少参数, 请检查配置.',
+));
