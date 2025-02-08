040f1e06348a39dc40df6e046b08e3c14a6766a7
Martin Tesar <martin.tesar@yourwifi.cz>
2025-02-08 20:11:09 +0100

    New Czech translation

---
+++ cs.dict.combodo-gantt-view.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2024 YOURWiFi s.r.o.
+ * @author	Martin Tesař <martin.tesar@yourwifi.cz>
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+ 
+Dict::Add('CS CZ', 'Czech', 'Čeština', array(
+	// Dictionary entries go here
+	'GanttDashlet/Label' => 'Gantt',
+	'GanttDashlet/Description' => 'See objects as a Gantt',
+	'GanttDashlet/NoPreviewAvailable' => 'Náhled není k dispozici!',
+	'GanttDashlet/Prop:DefaultTitle' => 'Tikety',
+	'GanttDashlet/Prop:DefaultTitle2' => 'Kontakty',
+
+	'GanttDashlet/Prop:Class' => 'Třída',
+	'GanttDashlet/Prop:Name' => 'Jméno',
+	'GanttDashlet/Prop:StartDate' => 'Datum zahájení~~',
+	'GanttDashlet/Prop:EndDate' => 'Datum ukončení~~',
+	'GanttDashlet/Prop:Title' => 'Název',
+	'GanttDashlet/Prop:DependsOn' => 'Závisí na',
+	'GanttDashlet/Prop:ColoredField' => 'Vybarveno dle',
+	'GanttDashlet/Prop:AdditionalInfoLeft' => 'Další informace 1',
+	'GanttDashlet/Prop:AdditionalInfoRight' => 'Další informace 2',
+	'GanttDashlet/Attribute:ActionDisabled' => 'Akce pro tento objekt zakázána',
+	'GanttDashlet:UI/Tab:GanttViewer' => "Ganttův pohled",
+	'GanttDashlet/Prop:ParentField' => 'Seskupit dle',
+	'GanttDashlet/Prop:Percentage' => 'Procento dokončení',
+	'GanttDashlet/Prop:GroupByInformations' => 'Seskupit dle informací',
+
+	'GanttDashlet/UI:CriticalPath' => 'Critical path',
+	'GanttDashlet/UI:ZoomIn' => 'Zvětšit',
+	'GanttDashlet/UI:ZoomOut' => 'Zmenšit',
+	'GanttDashlet/UI:MoveSplitBarRight' => 'Posunout dělicí lištu vpravo',
+	'GanttDashlet/UI:MoveSplitBarLeft' => 'Posunout dělicí lištu vlevo',
+	'GanttDashlet/UI:MoveSplitBarCenter' => 'Vycentrovat dělící lištu',
+
+	'GanttDashlet/Error:ParametersMissing' => 'Chybí některé parametry, zkontrolujte prosím konfiguraci.',
+));
